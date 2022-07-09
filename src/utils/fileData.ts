d9a7041b80368d4872724cafd502053fb595292a
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2022-07-09 15:43:28 +0200

    more dummy assets - looks like shit

--- src/utils/fileData.ts
+++ src/utils/fileData.ts
@@ -6,10 +6,34 @@ export const fileData: IFileData = {
       caption: 'NFT caption in education seciton',
       link: 'https://google.se',
     },
+    'nft_carbon_2.png': {
+      caption: 'NFT 2 caption in education seciton',
+      link: 'https://google.se',
+    },
+    'nft_carbon_3.png': {
+      caption: 'NFT 3 caption in education seciton',
+      link: 'https://google.se',
+    },
+    'nft_carbon_4.png': {
+      caption: 'NFT 4 caption in education seciton',
+      link: 'https://google.se',
+    },
   },
   experience: {
     'nft_carbon.png': {
       caption: 'NFT caption in experience seciton',
+      link: 'https://google.se',
+    },
+    'nft_carbon_2.png': {
+      caption: 'NFT 2 caption in experience seciton',
+      link: 'https://google.se',
+    },
+    'nft_carbon_3.png': {
+      caption: 'NFT 3 caption in experience seciton',
+      link: 'https://google.se',
+    },
+    'nft_carbon_4.png': {
+      caption: 'NFT 4 caption in experience seciton',
       link: 'https://google.se',
     },
   },
