555eb5b37b4283a13e390fcb802c8a5be3f9fd79
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2022-07-10 22:44:24 +0200

    chunking

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [glsl()],
   build: {
     target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules'))
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        },
+      },
+    },
   },
 });
