221c58c5b41a5374c6fed0212f4dd1d8520e8732
Christopher Bengtsson <christopher.bengtsson@trustly.com>
2022-08-04 23:48:52 +0200

    coment out colors for now

--- src/utils/categoriesCommonConfig.ts
+++ src/utils/categoriesCommonConfig.ts
@@ -1,26 +1,26 @@
 import { ICommonConfig } from '../types';
 
-const colorCombinations = [
-  [0x949398, 0xf4df4e],
-  [0xfc766a, 0x5b84b1],
-  [0x5f4b8b, 0xe69a8d],
-  [0x000000, 0xffffff],
-  [0x00203f, 0xadefd1],
-  [0x606060, 0xd6ed17],
-  [0x00539c, 0xeea47f],
-  [0x101820, 0xfee715],
-  [0x101820, 0xf2aa4c],
-  [0x2d2926, 0xe94b3c],
-  [0xffd662, 0x00539c],
-  [0x1c1c1b, 0xce4a7e],
-];
+// const colorCombinations = [
+//   [0x949398, 0xf4df4e],
+//   [0xfc766a, 0x5b84b1],
+//   [0x5f4b8b, 0xe69a8d],
+//   [0x000000, 0xffffff],
+//   [0x00203f, 0xadefd1],
+//   [0x606060, 0xd6ed17],
+//   [0x00539c, 0xeea47f],
+//   [0x101820, 0xfee715],
+//   [0x101820, 0xf2aa4c],
+//   [0x2d2926, 0xe94b3c],
+//   [0xffd662, 0x00539c],
+//   [0x1c1c1b, 0xce4a7e],
+// ];
 
-function shuffledColors() {
-  for (let i = colorCombinations.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [colorCombinations[i], colorCombinations[j]] = [colorCombinations[j], colorCombinations[i]];
-  }
-}
+// function shuffledColors() {
+//   for (let i = colorCombinations.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [colorCombinations[i], colorCombinations[j]] = [colorCombinations[j], colorCombinations[i]];
+//   }
+// }
 
 export const initialColor = 0x000000;
 export const categoriesCommonConfig: ICommonConfig = {
